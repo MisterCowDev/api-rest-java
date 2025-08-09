746511eb8d4608070c7e96f92902ae455b889ad1
mistercow <esteban.lagos21@gmail.com>
2025-08-09 00:58:34 -0400

    Agregado ID en Medico para poder cambiar sus datos correctamente a traves de PUT

--- src/main/java/med/voll/api/medico/DatosListaMedico.java
+++ src/main/java/med/voll/api/medico/DatosListaMedico.java
@@ -1,13 +1,17 @@
 package med.voll.api.medico;
 
 public record DatosListaMedico(
+        Long id,
         String nombre,
         String email,
         String documento,
         Especialidad especialidad
 ) {
+
+
     public DatosListaMedico(Medico medico) {
         this(
+                medico.getId(),
                 medico.getNombre(),
                 medico.getEmail(),
                 medico.getDocumento(),
