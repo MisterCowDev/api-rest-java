136ef6f0cdb21953387a51aa94b0ca6709179f05
mistercow <esteban.lagos21@gmail.com>
2025-08-11 23:00:24 -0400

    feat: add GestorErrores

---
+++ src/main/java/med/voll/api/infra/GestorErrores.java
@@ -0,0 +1,30 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.hibernate.action.internal.EntityActionVetoException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GestorErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity getionarError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity getionarError400(MethodArgumentNotValidException ex){
+        var errores = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errores.stream().map(DatosErrorValidacion::new).toList());
+    }
+
+    public record DatosErrorValidacion(String campo, String mensaje){
+        public DatosErrorValidacion(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
